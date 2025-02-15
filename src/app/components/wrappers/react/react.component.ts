a385109d6e2f098da6ccf94299b52ee1c1ff31a3
Michael Lowenstein <mlowenstein@collegehouse.com>
2025-02-14 22:02:17 -0700

    wrappers for vue and react in progress.

---
+++ src/app/components/wrappers/react/react.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mll-react',
+  standalone: true,
+  imports: [],
+  templateUrl: './react.component.html',
+  styleUrl: './react.component.scss'
+})
+export class ReactComponent {
+
+}
---
+++ src/app/components/wrappers/vue/vue.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mll-vue',
+  standalone: true,
+  imports: [],
+  templateUrl: './vue.component.html',
+  styleUrl: './vue.component.scss'
+})
+export class VueComponent {
+
+}
