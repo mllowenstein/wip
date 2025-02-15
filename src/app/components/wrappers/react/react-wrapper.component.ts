73abc976b987d653da8a6560c4501b953c7b42d0
Michael Lowenstein <mlowenstein@collegehouse.com>
2025-02-14 22:02:17 -0700

    wrappers for vue and react in progress.

---
+++ src/app/components/wrappers/react/react-wrapper.component.ts
@@ -0,0 +1,4 @@
+import { Component, ElementRef, AfterViewInit } from '@angular/core';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { ReactComponent } from './react.component';
