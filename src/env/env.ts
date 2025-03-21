8eea7d3f92b074c5a7e95d1eed5ffe9865c76ea4
Michael Lowenstein <mlowenstein@collegehouse.com>
2025-03-20 21:48:43 -0600

    env comments

--- src/env/env.ts
+++ src/env/env.ts
@@ -2,7 +2,7 @@ export const env: any = {
   production: false,
   tag: 'local',
   firebase: {
-    apiKey: "AIzaSyBO3usafUaemmViD7ayy6n3pg5AK86zq0s",
+    apiKey: "AIzaSyBO3usafUaemmViD7ayy6n3pg5AK86zq0s", // replace with your own config.
     authDomain: "wipp-73383.firebaseapp.com",
     databaseURL: "https://wipdata.firebaseio.com",
     projectId: "wipp-73383",
@@ -10,10 +10,10 @@ export const env: any = {
     messagingSenderId: "1092093745697",
     appId: "1:1092093745697:web:90eef2b2d35ee70b41a160",
     measurementId: "G-EPB2VXNNJ8",
-    databaseSecret: "zUfQO65fj1uxAhh24tDqQifJpNq1V3VOnSUPpkTw"
+    databaseSecret: "zUfQO65fj1uxAhh24tDqQifJpNq1V3VOnSUPpkTw" // replace with your own config.
   },
   perigon: {
-    apiKey: '41fe7898-8ec4-4480-9a62-c8347503c98a',
+    apiKey: '41fe7898-8ec4-4480-9a62-c8347503c98a', // replace with your own config.
     baseApiUrl: 'https://api.goperigon.com/v1/all',
     baseContentUrl: 'https://api.goperigon.com/v1/all?apiKey=41fe7898-8ec4-4480-9a62-c8347503c98a&q=Real%20Estate%20Legal%20in%20Canada'
   },
